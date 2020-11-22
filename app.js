181d6d15f05ea8b7ddaf72b896c701e5162d3dff
Shafee <shafeelinks@gmail.com>
2020-11-22 09:58:23 -0500

    advice slip working version first init

---
+++ app.js
@@ -0,0 +1,24 @@
+const adviceBody = document.querySelector('.advice-container');
+const button = document.querySelector('button');
+let p = document.querySelector('#filler');
+let advice;
+
+async function getAdvice() {
+    try {
+        const response = await axios.get('https://api.adviceslip.com/advice');
+        advice = response.data.slip.advice;
+    }
+    catch (err) {
+        console.log('Cats exploded', err);
+    }
+
+};
+
+getAdvice();
+
+button.addEventListener('click', () => {
+    setTimeout(() => {
+        getAdvice();
+        p.innerText = advice;
+    }, 600);
+});
